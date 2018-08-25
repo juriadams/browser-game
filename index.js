1ac1868c34c9de9025a83b42982bb2ca52d758a7
Juri Adams <juri.adams@inexio.net>
2018-08-25 17:06:38 +0200

    Asset update

--- index.js
+++ index.js
@@ -35,6 +35,49 @@ function keyboard(keyCode) {
     return key;
 }
 
+function hitTestRectangle(r1, r2) {
+
+    //Define the variables we'll need to calculate
+    let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+
+    //hit will determine whether there's a collision
+    hit = false;
+
+    //Find the center points of each sprite
+    r1.centerX = r1.x + r1.width / 2;
+    r1.centerY = r1.y + r1.height / 2;
+    r2.centerX = r2.x + r2.width / 2;
+    r2.centerY = r2.y + r2.height / 2;
+
+    //Find the half-widths and half-heights of each sprite
+    r1.halfWidth = r1.width / 2;
+    r1.halfHeight = r1.height / 2;
+    r2.halfWidth = r2.width / 2;
+    r2.halfHeight = r2.height / 2;
+
+    //Calculate the distance vector between the sprites
+    vx = r1.centerX - r2.centerX;
+    vy = r1.centerY - r2.centerY;
+
+    //Figure out the combined half-widths and half-heights
+    combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+    combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+    //Check for a collision on the x axis
+    if (Math.abs(vx) < combinedHalfWidths) {
+
+        //A collision might be occuring. Check for a collision on the y axis
+        hit = Math.abs(vy) < combinedHalfHeights;
+    } else {
+
+        //There's no collision on the x axis
+        hit = false;
+    }
+
+    //`hit` will be either `true` or `false`
+    return hit;
+}
+
 var type = "WebGL";
 if(!PIXI.utils.isWebGLSupported()){
     type = "canvas"
@@ -45,12 +88,12 @@ PIXI.utils.sayHello(type);
 // Create the application
 // TODO: Make size of browser window
 var app = new PIXI.Application({
-    width: 800,
+    width: 1000,
     height: 600,
     antialias: true
 });
 
-var player1, player2, state;
+var player1, player2, goal, state;
 
 var difficulty = 5;
 
@@ -58,10 +101,22 @@ var difficulty = 5;
 app.renderer.backgroundColor = 0x061639;
 
 // Loading all images and then calling setup() function
-PIXI.loader.add(['assets/images/minion.png']).load(setup);
+PIXI.loader.add(['assets/images/minion.png', 'assets/images/bg.png', 'assets/images/player3.png', 'assets/images/goal.png']).load(setup);
 
 // Adding the players to the stage
 function setup() {
+
+    // Loading in background
+    let background = new PIXI.Sprite(PIXI.loader.resources['assets/images/bg.png'].texture);
+    background.width = app.screen.width;
+    background.height = app.screen.height;
+    app.stage.addChild(background);
+
+    goal = new PIXI.Sprite(PIXI.loader.resources['assets/images/goal.png'].texture);
+    goal.anchor.set(0.5);
+    goal.x = app.screen.width - goal.width / 2;
+    goal.y = app.screen.height / 2;
+    app.stage.addChild(goal);
 
     // Loading and adding player textures to canvas
     player1 = new PIXI.Sprite(PIXI.loader.resources['assets/images/minion.png'].texture);
@@ -71,18 +126,18 @@ function setup() {
 
     // Setting up player1
     player1.anchor.set(0.5);
-    player1.height = app.screen.height / 10;
-    player1.width = app.screen.height / 10;
-    player1.x = 100;
+    player1.height = 100;
+    player1.width = 100;
+    player1.x = 50;
     player1.y = app.screen.height / 2 - app.screen.height / 10;
     player1.vx = 0; // Setting velocity
     player1.vy = 0; // Setting velocity
 
     // Setting up player2
     player2.anchor.set(0.5);
-    player2.height = app.screen.height / 10;
-    player2.width = app.screen.height / 10;
-    player2.x = 100;
+    player2.height = 100;
+    player2.width = 100;
+    player2.x = 50;
     player2.y = app.screen.height / 2 + app.screen.height / 10;
     player2.vx = 0; // Setting velocity
     player2.vy = 0; // Setting velocity
@@ -101,12 +156,14 @@ function gameLoop(delta){
 }
 
 function play(delta) {
-    // TODO: Sync velocity with IPS (input per second)
-    // player1.vx = 1;
-    // player1.x += player1.vx;
-
-    // player2.vx = 2;
-    // player2.x += player2.vx;
+    if (hitTestRectangle(player1, goal)) {
+        console.log('Player 1 won!');
+        state = end;
+    }
+    if (hitTestRectangle(player2, goal)) {
+        console.log('Player 2 won!');
+        state = end;
+    }
 }
 
 function pause(delta) {
@@ -129,9 +186,9 @@ function resetGame() {
     state = pause;
 
     // Resetting the players' positions and rotations
-    player1.x = 100;
+    player1.x = 50;
     player1.rotation = 0;
-    player2.x = 100;
+    player2.x = 50;
     player2.rotation = 0;
 }
 
